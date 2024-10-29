eb42a2bc519352f64a41bfa40fdd5a65396ac745
TheLastCookie-404 <se660745@gmail.com>
2024-10-29 15:43:57 +0000

    Recursive component

---
+++ src/components/RecursiveComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-for="elem in content">
+    <div v-if="(typeof elem) == 'object'">
+      <RecursiveComponent recursion :content="elem"/>
+    </div>
+    <div class="outline" v-else>{{ elem }}</div>
+  </div>
+</template>
+
+<script setup>
+import RecursiveComponent from './RecursiveComponent.vue'
+const props = defineProps({
+  content: {
+    type: Number,
+    defult: 0
+  }
+});
+</script>
--- src/components/TestComponent.vue
+++ src/components/TestComponent.vue
@@ -1,15 +1,26 @@
 <template>
-  <div class="bg-base-100 p-1">
+  <div class="bg-base-100 mb-3 p-1">
     <span>I`m parential component</span>
     <lol />
   </div>
+  <RecursiveComponent :content="obj" />
 </template>
 
 <script setup>
+import RecursiveComponent from '~/components/RecursiveComponent.vue'
 const props = defineProps({
   lol: {
     type: Object,
     required: false
   }
 });
+const obj = {
+  "lol": {
+    1: "kek",
+    "omg": {
+      "oof": [1, 64, 34],
+      9: "azaza"
+    }
+  }
+};
 </script>

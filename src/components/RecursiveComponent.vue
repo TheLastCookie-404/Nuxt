748867fd4e28785bef21a9eabe9abea249cc4b80
The Last Cookie <74693705+TheLastCookie-404@users.noreply.github.com>
2024-11-02 13:35:15 +0500

    Lol

--- src/components/RecursiveComponent.vue
+++ src/components/RecursiveComponent.vue
@@ -1,14 +1,15 @@
 <template>
-  <div v-for="(elem, index) in content" :key="index">
-    <div v-if="(typeof elem) == 'object'">
-      <RecursiveComponent recursion :content="elem"/>
+  <div>
+    <div v-for="(elem, index) in content" :key="index">
+      <div v-if="(typeof elem) == 'object'">
+        <RecursiveComponent recursion :content="elem"/>
+      </div>
+      <div class="outline" v-else>{{ `${index}: ${elem}` }}</div>
     </div>
-    <div class="outline" v-else>{{ `${index}: ${elem}` }}</div>
   </div>
 </template>
 
 <script setup>
-import RecursiveComponent from './RecursiveComponent.vue'
 const props = defineProps({
   content: {
     type: Object,

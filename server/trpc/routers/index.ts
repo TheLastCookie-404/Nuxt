c8e2db551cf6f8f22a5b8d87f2114efbe1538495
The Last Cookie <74693705+TheLastCookie-404@users.noreply.github.com>
2025-04-25 21:51:32 +0500

    TRPC work

--- server/trpc/routers/index.ts
+++ server/trpc/routers/index.ts
@@ -1,8 +1,19 @@
-import { baseProcedure, createTRPCRouter } from "~~/server/trpc/init";
+import { baseProcedure, createTRPCRouter } from "~/server/trpc/init";
 import { z } from "zod";
 
 export const appRouter = createTRPCRouter({
   hello: baseProcedure
+    .input(
+      z.object({
+        text: z.string(),
+      })
+    )
+    .query((opts) => {
+      return {
+        greeting: `hello ${opts.input.text}`,
+      };
+    }),
+  test: baseProcedure
     .input(
       z.object({
         text: z.string(),
